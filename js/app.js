1d0b9e1b0fb1060d7167f5d2ca076551b57d24e1
Gabriel Victor Gomes <gavitogomes55@gmail.com>
2020-10-01 02:02:12 -0300

    Changes on the structure of the project. Now there are lists of todos, that can be added, edited and removed. The lists also have a color that can be set on the creation and changed on the edition.

--- js/app.js
+++ js/app.js
@@ -1,125 +1,228 @@
-/*global Vue, todoStorage */
+/*global Vue, todoStorage, list */
 
 (function (exports) {
+    "use strict";
 
-	'use strict';
+    var filters = {
+        all: function (list) {
+            return list;
+        },
+        active: function (list) {
+            return list.filter(function (todo) {
+                return !todo.completed;
+            });
+        },
+        completed: function (list) {
+            return list.filter(function (todo) {
+                return todo.completed;
+            });
+        },
+    };
 
-	var filters = {
-		all: function (todos) {
-			return todos;
-		},
-		active: function (todos) {
-			return todos.filter(function (todo) {
-				return !todo.completed;
-			});
-		},
-		completed: function (todos) {
-			return todos.filter(function (todo) {
-				return todo.completed;
-			});
-		}
-	};
+    exports.app = new Vue({
+        // the root element that will be compiled
+        el: ".todoapp",
 
-	exports.app = new Vue({
+        // app initial state
+        data: {
+            lists: todoStorage.fetch(),
+            newTodo: null,
+            newTodoListId: null,
+            editedTodo: null,
+            newList: null,
+            editedList: null,
+            activeFilters: [],
+            colors: [
+                "#EE7F7F",
+                "#AEAEAE",
+                "#F2CC73",
+                "#AC96D2",
+                "#7EB4D4",
+                "#7ED48A",
+                "#EEAB7F",
+            ],
+        },
 
-		// the root element that will be compiled
-		el: '.todoapp',
+        // watch todos change for localStorage persistence
+        watch: {
+            lists: {
+                deep: true,
+                handler: todoStorage.save,
+            },
+        },
 
-		// app initial state
-		data: {
-			todos: todoStorage.fetch(),
-			newTodo: '',
-			editedTodo: null,
-			visibility: 'all'
-		},
+        // created is called right after the instance is created
+        created: function () {
+            for (let i = 0; i < this.lists.length; i++) {
+                this.activeFilters.push("all");
+            }
+        },
 
-		// watch todos change for localStorage persistence
-		watch: {
-			todos: {
-				deep: true,
-				handler: todoStorage.save
-			}
-		},
+        // methods that implement data logic.
+        // note there's no DOM manipulation here at all.
+        methods: {
+            addTodo: function (id) {
+                this.newTodo = "";
+                this.newTodoListId = id;
+            },
 
-		// computed properties
-		// http://vuejs.org/guide/computed.html
-		computed: {
-			filteredTodos: function () {
-				return filters[this.visibility](this.todos);
-			},
-			remaining: function () {
-				return filters.active(this.todos).length;
-			},
-			allDone: {
-				get: function () {
-					return this.remaining === 0;
-				},
-				set: function (value) {
-					this.todos.forEach(function (todo) {
-						todo.completed = value;
-					});
-				}
-			}
-		},
+            doneAddTodo: function () {
+                var value = this.newTodo && this.newTodo.trim();
+                if (!value) {
+                    return;
+                }
+                this.lists[this.newTodoListId].todos.push({
+                    id: this.lists[this.newTodoListId].length,
+                    title: value,
+                    completed: false,
+                });
+                this.newTodo = null;
+                this.newTodoListId = null;
+            },
 
-		// methods that implement data logic.
-		// note there's no DOM manipulation here at all.
-		methods: {
+            cancelAddTodo: function () {
+                this.newTodo = null;
+                this.newTodoListId = null;
+            },
 
-			pluralize: function (word, count) {
-				return word + (count === 1 ? '' : 's');
-			},
+            removeTodo: function (listId, todo) {
+                var index = this.lists[listId].todos.indexOf(todo);
+                this.lists[listId].todos.splice(index, 1);
+            },
 
-			addTodo: function () {
-				var value = this.newTodo && this.newTodo.trim();
-				if (!value) {
+            editTodo: function (todo) {
+                this.beforeEditCache = todo.title;
+                this.editedTodo = todo;
+            },
+
+            doneEditTodo: function (todo) {
+                if (!this.editedTodo) {
+                    return;
+                }
+                this.editedTodo = null;
+                todo.title = todo.title.trim();
+                if (!todo.title) {
+                    this.removeTodo(todo);
+                }
+            },
+
+            cancelEditTodo: function (todo) {
+                this.editedTodo = null;
+                todo.title = this.beforeEditCache;
+            },
+
+            removeCompleted: function (listId) {
+                this.lists[listId].todos = filters.active(
+                    this.lists[listId].todos
+                );
+            },
+
+            addList: function () {
+                this.newList = {
+                    title: "",
+                    color: this.colors[0],
+                };
+            },
+
+            doneAddList: function () {
+                var value = this.newList.title && this.newList.title.trim();
+
+                var alreadyExists =
+                    this.lists.filter(function (list) {
+                        return list.title == value;
+                    }).length > 0;
+
+                if (!value || alreadyExists) {
+                    this.newList = null;
+                    return;
+                }
+                this.lists.push({
+                    id: this.lists.length,
+                    title: value,
+                    color: this.newList.color,
+                    todos: [],
+                });
+                this.newList = null;
+                this.activeFilters.push("all");
+            },
+
+            cancelAddList: function () {
+                this.newList = null;
+            },
+
+            editList: function (index) {
+                this.editedList = {
+                    id: index,
+                    title: this.lists[index].title,
+                    color: this.lists[index].color,
+                    todos: this.lists[index].todos,
+                };
+            },
+
+            doneEditList: function () {
+                var value =
+                    this.editedList.title && this.editedList.title.trim();
+
+                if (!value) {
+					this.removeList(this.editedList.id);
+					this.editedList = null;
 					return;
-				}
-				this.todos.push({ id: this.todos.length + 1, title: value, completed: false });
-				this.newTodo = '';
-			},
+                }
+                this.lists[this.editedList.id] = this.editedList;
+                this.editedList = null;
+            },
 
-			removeTodo: function (todo) {
-				var index = this.todos.indexOf(todo);
-				this.todos.splice(index, 1);
-			},
+            cancelEditList: function () {
+                this.editedList = null;
+            },
 
-			editTodo: function (todo) {
-				this.beforeEditCache = todo.title;
-				this.editedTodo = todo;
-			},
+            removeList: function (index) {
+                this.lists.splice(index, 1);
+            },
 
-			doneEdit: function (todo) {
-				if (!this.editedTodo) {
-					return;
-				}
-				this.editedTodo = null;
-				todo.title = todo.title.trim();
-				if (!todo.title) {
-					this.removeTodo(todo);
-				}
-			},
+            filterList: function (listId, filter) {
+                return filter && filters[filter](this.lists[listId].todos);
+            },
 
-			cancelEdit: function (todo) {
-				this.editedTodo = null;
-				todo.title = this.beforeEditCache;
-			},
+            completeAll: function (listId) {
+                var value =
+                    filters.completed(this.lists[listId].todos).length == 0;
+                console.log(value);
+                this.lists[listId].todos.map(function (todo) {
+                    todo.completed = value;
+                });
+            },
 
-			removeCompleted: function () {
-				this.todos = filters.active(this.todos);
-			}
-		},
+            selectActiveFilter: function (listId, filter) {
+                this.activeFilters[listId] = filter;
+                this.$forceUpdate();
+            },
+        },
 
-		// a custom directive to wait for the DOM to be updated
-		// before focusing on the input field.
-		// http://vuejs.org/guide/custom-directive.html
-		directives: {
-			'todo-focus': function (el, binding) {
-				if (binding.value) {
-					el.focus();
-				}
-			}
-		}
-	});
+        // a custom directive to wait for the DOM to be updated
+        // before focusing on the input field.
+        // http://vuejs.org/guide/custom-directive.html
+        directives: {
+            "todo-focus": function (el, binding) {
+                if (binding.value) {
+                    el.focus();
+                }
+            },
+        },
 
+        // filters for text formatting
+        // https://vuejs.org/v2/guide/filters.html
+
+        filters: {
+            capitalize: function (value) {
+                if (!value) return "";
+                value = value.toString();
+                return value.charAt(0).toUpperCase() + value.slice(1);
+            },
+
+            pluralize: function (value, count) {
+                return value + (count == 1 ? "" : "s");
+            },
+        },
+    });
 })(window);
--- js/routes.js
+++ js/routes.js
@@ -6,16 +6,9 @@
 
 	var router = new Router();
 
-	['all', 'active', 'completed'].forEach(function (visibility) {
-		router.on(visibility, function () {
-			app.visibility = visibility;
-		});
-	});
-
 	router.configure({
 		notfound: function () {
 			window.location.hash = '';
-			app.visibility = 'all';
 		}
 	});
 
